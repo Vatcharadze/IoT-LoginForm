e86bcc235caa43476cc14d5c939f34598cfd50ab
Davit Vatcharadze <dato.vatcharadze@gmail.com>
2025-11-07 20:02:28 +0400

    Add files via upload

---
+++ LoginWithJsonApp/LoginWithJsonApp/Form2.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace LoginWithJsonApp
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            timer = new System.Windows.Forms.Timer(components);
+            lblTimer = new Label();
+            button1 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            SuspendLayout();
+            // 
+            // timer
+            // 
+            timer.Enabled = true;
+            timer.Interval = 1000;
+            timer.Tick += timer1_Tick;
+            // 
+            // lblTimer
+            // 
+            lblTimer.AutoSize = true;
+            lblTimer.Location = new Point(12, 19);
+            lblTimer.Name = "lblTimer";
+            lblTimer.Size = new Size(0, 15);
+            lblTimer.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(180, 248);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 36);
+            button1.TabIndex = 1;
+            button1.Text = "Reset Timer!";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Red;
+            label1.Location = new Point(140, 49);
+            label1.Name = "label1";
+            label1.Size = new Size(142, 30);
+            label1.TabIndex = 3;
+            label1.Text = "Fire Detected!";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.Red;
+            label2.Location = new Point(140, 91);
+            label2.Name = "label2";
+            label2.Size = new Size(171, 30);
+            label2.TabIndex = 4;
+            label2.Text = "Smoke Detected!";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.Red;
+            label3.Location = new Point(140, 131);
+            label3.Name = "label3";
+            label3.Size = new Size(151, 30);
+            label3.TabIndex = 5;
+            label3.Text = " LPG Detected!";
+            label3.Click += label3_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 8.25F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            label4.ForeColor = SystemColors.ControlDarkDark;
+            label4.Location = new Point(1, 289);
+            label4.Name = "label4";
+            label4.Size = new Size(232, 13);
+            label4.TabIndex = 6;
+            label4.Text = "Window Will Close In 20 Seconds! (Changeable)";
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(484, 311);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Controls.Add(lblTimer);
+            Name = "Form2";
+            Text = "ControlRoom";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer;
+        private Label lblTimer;
+        private Button button1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+    }
+}
---
+++ LoginWithJsonApp/LoginWithJsonApp/Form2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace LoginWithJsonApp
+{
+    public partial class Form2 : Form
+    {
+        private int seconds = 0;
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            seconds++;
+            lblTimer.Text = "Timer: " + seconds + " seconds";
+            if (seconds == 20)
+            {
+                Close();
+            }
+        }
+
+        private void Form2_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            seconds = 0;
+            lblTimer.Text = "Timer: 0 seconds";
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+
+
+}
